9a6d84975cc096a7159d7b7b4ca86fd2b613067c
전현태 <jeonht043@gmail.com>
2023-02-15 17:21:19 +0900

    3주차

---
+++ 4주차/Main_백준_10971_외판원순회2_전현태_96ms.java
@@ -0,0 +1,46 @@
+package study.day0215;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_백준_10971_외판원순회2_전현태_96ms {
+	static int [][] map;
+	static int N, isSelected = 0;
+	static int ans = 2147483647;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		map = new int [N][N];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		for (int n = 0; n < N; n++) {//시작지점
+			isSelected = isSelected | (1 << n);
+			check(n, n, 0, 0);
+			isSelected = isSelected - (1 << n);
+		}
+		System.out.println(ans);
+	}
+	private static void check(int first, int now, int cnt, int Weight) {
+		if (cnt == N-1) {
+			int lastWeight = Weight + map[now][first];
+			if (ans > lastWeight && map[now][first] != 0)
+				ans = lastWeight;
+			return;
+		}
+		for (int i = 0; i < N; i++) {// 순열과 똑같음
+			if ((isSelected & (1 << i)) != 0 || map[now][i] == 0)continue;
+			isSelected = isSelected | (1 << i);
+			int temp = Weight + map[now][i];
+			if (temp < ans)
+				check(first, i, cnt+1, temp);
+			isSelected = isSelected - (1 << i);
+		}
+	}
+	
+}
---
+++ 4주차/Main_백준_1717_집합의표현_메모리초과.java
@@ -0,0 +1,50 @@
+package study.day0215;
+
+/** 메모리 초과*/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main_백준_1717_집합의표현_메모리초과 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		Map<Integer, Set<Integer>> arr = new HashMap<Integer, Set<Integer>>();// 각 원소는 본인이 속한 집합을 들고다님
+		for (int i = 0; i < N+1; i++) {
+			Set<Integer> add = new HashSet<>();
+			add.add(i);
+			arr.put(i, add);
+		}
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int checker = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			if (checker == 0) {// 각각이 집합을 들고 다니기 때문에 a,b 둘다 집합에 서로 값을 넣어줌
+				Set<Integer> temp = arr.get(b);
+				for (Integer num : arr.get(a)) {
+					temp.add(num);
+				}
+				arr.put(b, temp);
+				temp = arr.get(a);
+				for (Integer num : arr.get(b)) {
+					temp.add(num);
+				}
+				arr.put(a, temp);
+			}
+			else {
+				if (arr.get(a).contains(b))// a집합에 b가포함 되었는지
+					System.out.println("YES");
+				else
+					System.out.println("NO");
+			}
+		}
+	}
+}
